821557b17dc43b4e10efb26b529733142eb60248
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-06-13 12:39:08 -0300

    update

---
+++ aula11/ex008.js
@@ -0,0 +1,16 @@
+// Condição simples
+
+var vel = 60.5
+
+console.log(`A velocidade do carro é ${vel} km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança.`)
+
+
+
+
+
---
+++ aula11/ex009.js
@@ -0,0 +1,12 @@
+// Condição composta
+
+var pais = 'França'
+
+console.log(`Vivendo no ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro.')
+} else {
+    console.log('Você é estrangeiro!')
+}
+
+
