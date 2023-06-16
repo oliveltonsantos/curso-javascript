93b0cbf228e2a2a8d5368c3f6c8da6d632c82360
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-06-16 13:23:09 -0300

    update

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+// Teste lógico no início
+
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // (isso é igual a c = c + 1)
+}
+
+// Teste lógico no final
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // (isso é igual a c = c + 1)
+} while (c <= 6)
+
---
+++ aula13/node_5b321e1a00d37.tmp.js
@@ -0,0 +1,9 @@
+// Teste lógico no início
+
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++ // (isso é igual a c = c + 1)
+}
+
+ 
