f0ca91e100c76010d14913b11dbc656a16a1fafb
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-06-18 23:48:21 -0300

    update

---
+++ aula14/node_d591a89b782d1.tmp.js
@@ -0,0 +1,15 @@
+/*
+
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++ // c = c + 1
+}
+
+*/
+
+// Repetição com variável de controle
+
+for (var c = 1; c <= 3; c++) {
+    console.log(c)
+}
